0c3aeffef705be6052dcfa1a61c9b546f400074a
David Starr <david@elegantcode.com>
2024-12-03 16:38:09 -0800

    showing selected text or fault message

--- ec-code-assist/src/commands/HelloWorldCommand.ts
+++ ec-code-assist/src/commands/HelloWorldCommand.ts
@@ -3,7 +3,7 @@ import { Command } from './command';
 
 export class HelloWorldCommand implements Command {
     
-    name: string = 'ec-code-assist.helloWorld';
+    readonly name: string = 'ec-code-assist.helloWorld';
 
     execute(context: vscode.ExtensionContext, args?: string[]): void {
         vscode.window.showInformationMessage('Hello World from EC Code Assist command!');
--- ec-code-assist/src/commands/ShowSelectedCodeCommand.ts
+++ ec-code-assist/src/commands/ShowSelectedCodeCommand.ts
@@ -3,16 +3,23 @@ import { Command } from './command';
 
 export class ShowSelectedCodeCommand implements Command {
     
-    name: string = 'ec-code-assist.showSelectedCode';
+	// The command identifier
+    readonly name: string = 'ec-code-assist.showSelectedCode';
 
     execute(context: vscode.ExtensionContext, args?: string[]): void {
      
         const editor = vscode.window.activeTextEditor;
+
 		if (editor) {
 			const document = editor.document;
 			const selection = editor.selection;
-			const text = document.getText(selection);
-			vscode.window.showInformationMessage('Selected text is: ' + text);
+
+			if (selection.isEmpty) {
+				vscode.window.showInformationMessage('No text selected');
+			} else {
+				const text = document.getText(selection);
+				vscode.window.showInformationMessage('Selected text is: ' + text);
+			}
 		}
     }
 }
