57ca66b2d5597a0b310e55299d74c787af92a765
David Starr <david@elegantcode.com>
2024-12-07 06:07:13 -0800

    removing comment

--- ec-code-assist/src/commands/index.ts
+++ ec-code-assist/src/commands/index.ts
@@ -1,5 +1,4 @@
 // export all the commands from the commands folder
-// export * from './commands';
 export { Command } from './Command';
 export { HelloWorldCommand } from './HelloWorldCommand';
 export { ShowCodeCommand } from './ShowCodeCommand';
